193d4621fd821196855fee5c9048a6a9878b5df2
Zantiki <darealzantiki@gmail.com>
2024-01-18 19:10:17 +0100

    Feature: SodaActions enum

--- soda_machine_py/soda_machine.py
+++ soda_machine_py/soda_machine.py
@@ -1,8 +1,12 @@
 import abc
+import string
+from enum import Enum
+from typing import Dict
 
 
 class Soda:
 
+    @property
     @classmethod
     def name(cls):
         return cls.__name__.lower()
@@ -25,11 +29,40 @@ class Sprite(Soda):
     price = 15.00
 
 
+class SodaActions(Enum):
+    insert = "insert"
+    order = "order"
+    sms_order = "sms_order"
+    recall = "recall"
+
+
 class SodaMachine:
-    pass
+
+    def __init__(self, inventory: Dict[string, int]):
+        if not inventory:
+            inventory = SodaMachine.get_default_inventory()
+
+        self.inventory = inventory
+        self.internal_cash = 0
+        self.kill = False
+
+    @staticmethod
+    def get_default_inventory():
+        default_inventory = {
+            Coke.name: 5,
+            Sprite.name: 3,
+            Fanta.name: 3
+        }
+        return default_inventory
+
+    def show_instructions(self):
+        pass
 
     def start(self):
-        pass
+
+        while not self.kill:
+            pass
+
 
 
 if __name__ == "__main__":
