826b055ca4a3eb9c32e91b00622e9ce34b58cd1a
Zantiki <55238744+Zantiki@users.noreply.github.com>
2024-01-21 17:16:24 +0100

    Refactored soda classes into soda_machine.py

--- soda_machine_py/soda_machine.py
+++ soda_machine_py/soda_machine.py
@@ -5,7 +5,43 @@ from enum import Enum
 from time import sleep
 from typing import Dict
 
-from soda_machine_py.soda import Coke, Sprite, Fanta, SodaFactory
+
+# from soda_machine_py.soda import Coke, Sprite, Fanta, SodaFactory
+class Soda:
+
+    @classmethod
+    @property
+    def name(cls):
+        return cls.__name__.lower()
+
+    @property
+    @abc.abstractmethod
+    def price(self):
+        pass
+
+
+class Coke(Soda):
+    price = 20.00
+
+
+class Fanta(Soda):
+    price = 15.00
+
+
+class Sprite(Soda):
+    price = 15.00
+
+
+class SodaFactory:
+    _types = {
+        Coke.name: Coke(),
+        Sprite.name: Sprite(),
+        Fanta.name: Fanta()
+    }
+
+    @staticmethod
+    def get_by_name(soda_name: str):
+        return SodaFactory._types[soda_name]
 
 
 class SodaActions(Enum):
